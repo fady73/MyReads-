53725d40aae0fc1b8edd29963a82ba577e152861
fady <fadykhayrat@gmail.com>
2020-03-03 00:32:29 +0200

    format

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -8,52 +8,51 @@ class Search extends Component {
   static propTypes = {
     allSelectedBooks: PropTypes.array,
     results: PropTypes.array,
-    onBookChanged:PropTypes.func.isRequired,
-    searchQuery:PropTypes.string
-    
+    onBookChanged: PropTypes.func.isRequired,
+    searchQuery: PropTypes.string
+
   };
-    state = {
-    	allSelectedBooks: [],
-    	results: [],
-    	searchQuery: ""
-    }
-    searchQuery = (query) => {
-      console.log(query)
-    	this.setState({searchQuery: query,allSelectedBooks:this.props.allBooks}, this.searchBooks)
-    }
-   rememberShelfs(result){
-      result.forEach(allBooks => {
-        let selectedBooks = this.state.allSelectedBooks.filter(Book => Book.id === allBooks.id)
-        selectedBooks.map((e) => {
-          return allBooks.shelf = e.shelf
-        })
+  state = {
+    allSelectedBooks: [],
+    results: [],
+    searchQuery: ""
+  }
+  searchQuery = (query) => {
+    console.log(query)
+    this.setState({ searchQuery: query, allSelectedBooks: this.props.allBooks }, this.searchBooks)
+  }
+  rememberShelfs(result) {
+    result.forEach(allBooks => {
+      let selectedBooks = this.state.allSelectedBooks.filter(Book => Book.id === allBooks.id)
+      selectedBooks.map((e) => {
+        return allBooks.shelf = e.shelf
+      })
     })
   }
-    searchBooks() {
-    	if(this.state.searchQuery === '' || this.state.searchQuery === undefined) {
-    		return this.setState({ results: [] })
-      }
-      if(this.state.searchQuery.trim().length>0)
-      {    	
+  searchBooks() {
+    if (this.state.searchQuery === '' || this.state.searchQuery === undefined) {
+      return this.setState({ results: [] })
+    }
+    if (this.state.searchQuery.trim().length > 0) {
       BookApi.search(this.state.searchQuery.trim())
-    	.then(result => {
-    		if(result.error) {
-    			return this.setState({ results: [] })
-    		}
-    		else {
-          this.rememberShelfs(result);
-        }
-    		this.setState({results: result})
-      })
+        .then(result => {
+          if (result.error) {
+            return this.setState({ results: [] })
+          }
+          else {
+            this.rememberShelfs(result);
+          }
+          this.setState({ results: result })
+        })
     }
-    }
-  
+  }
+
 
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-        <Link className="close-search" to="/">Close</Link>
+          <Link className="close-search" to="/">Close</Link>
 
           <div className="search-books-input-wrapper">
             {/*
@@ -65,16 +64,17 @@ class Search extends Component {
                 you don't find a specific author or title. Every search is limited by search terms.
                 */}
             <input type="text" placeholder="Search by title or author" value={this.state.searchQuery}
-                	onChange={(event) => {  
-                  this.searchQuery(event.target.value)}} />
+              onChange={(event) => {
+                this.searchQuery(event.target.value)
+              }} />
 
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-          { this.state.searchQuery !== "" && this.state.results.map((book) =>
-           <BookItem key={book.id}   onBookChanged={this.props.onBookChanged} shelf="none" book={book} />) }
-          {/* { this.state.searchQuery===""||this.state.results.length<=0&&<p>No search result for this query try again</p>} */}
+            {this.state.searchQuery !== "" && this.state.results.map((book) =>
+              <BookItem key={book.id} onBookChanged={this.props.onBookChanged} shelf="none" book={book} />)}
+            {/* { this.state.searchQuery===""||this.state.results.length<=0&&<p>No search result for this query try again</p>} */}
 
           </ol>
         </div>
